af8bf860cd5aed89f0afc26476e158559b106e7f
luozuda <43851221+luozuda@users.noreply.github.com>
2019-03-12 11:52:03 +0800

    luozuda

--- vue/online-translation/src/components/TranslateForm.vue
+++ vue/online-translation/src/components/TranslateForm.vue
@@ -3,7 +3,7 @@
     <form v-on:submit="formSubmit">
       <input class="form-control" type="text" v-model="textToTranslate" placeholder="输入需要翻译的内容">
       <select class="form-control" v-model="language">
-        <option value="en" selected>English</option>
+        <option value="en">English</option>
         <option value="ru">Russian</option>
         <option value="ko">Korean</option>
         <option value="ja">Janpenese</option>
