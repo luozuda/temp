2ed3c09a86eda1d64f0447a695f54c394e0bc0df
luozuda <43851221+luozuda@users.noreply.github.com>
2019-03-17 13:53:56 +0800

    luozuda

---
+++ vue/news/src/components/Content.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="content">
+    <ul class="dataList">
+      <!-- <li v-for="data in dataList" :key="data.uniquekey">{{data.title}}</li> -->
+      <li v-for="data in dataList" :key="data.uniquekey">
+        <a class="link" :href="data.url">
+          <h4 v-text="data.title"></h4>
+          <ul class="imgList">
+            <li>
+              <img :src="data.thumbnail_pic_s" alt>
+            </li>
+            <li>
+              <img :src="data.thumbnail_pic_s02" alt>
+            </li>
+            <li>
+              <img :src="data.thumbnail_pic_s03" alt>
+            </li>
+          </ul>
+          <p>{{data.author_name}}&nbsp;{{data.date}}</p>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Content",
+  props: ["dataList"]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#content {
+  height: 100%;
+  overflow-y: scroll;
+}
+.dataList > li {
+  border-bottom: 1px solid #eee;
+}
+.link {
+  display: block;
+  padding: 0 0.5rem;
+  color: #000;
+}
+.link > h4,
+.link > p {
+  padding: 0.2rem 0;
+}
+.link > h4 {
+  font-size: 1.1rem;
+}
+.link > p {
+  color: #aaa;
+}
+.imgList {
+  column-count: 3;
+  column-gap: 0.2rem;
+}
+.imgList > li > img {
+  width: 100%;
+}
+</style>
---
+++ vue/news/src/components/Navigator.vue
@@ -0,0 +1,76 @@
+<template>
+  <nav id="navigator">
+    <ul class="list">
+      <li
+        v-for="(item,index) in items"
+        :key="item.name"
+        :class="{ active : item.isActive}"
+        @click="changeClass(index)"
+      >{{item.name}}</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navigator",
+  data: function() {
+    return {
+      items: [
+        { name: "头条", type: "top", isActive: false },
+        { name: "社会", type: "shehui", isActive: false },
+        { name: "国内", type: "guonei", isActive: false },
+        { name: "国际", type: "guoji", isActive: false },
+        { name: "娱乐", type: "yule", isActive: false },
+        { name: "体育", type: "tiyu", isActive: false },
+        { name: "军事", type: "junshi", isActive: false },
+        { name: "科技", type: "keji", isActive: false },
+        { name: "财经", type: "caijing", isActive: false },
+        { name: "时尚", type: "shishang", isActive: false }
+      ]
+    };
+  },
+  methods: {
+    changeClass: function(index) {
+      this.items.forEach(function(item) {
+        item.isActive = false;
+      });
+      this.items[index].isActive = true;
+      this.$emit("select", this.items[index].type);
+    }
+  },
+  created: function() {
+    //实例被创建后
+    this.items[0].isActive = true;
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#navigator {
+  background-color: #fff;
+  overflow-x: scroll;
+}
+@media screen and (min-width: 25rem) {
+  #navigator {
+    overflow: hidden;
+  }
+}
+.list {
+  white-space: nowrap;
+  line-height: 0;
+}
+.list li {
+  display: inline-block;
+  padding: 0 0.5rem;
+  height: 2.5rem;
+  line-height: 2.5rem;
+  text-align: center;
+  font-weight: 700;
+  color: #333;
+}
+.list .active {
+  color: rgb(201, 53, 53);
+}
+</style>
