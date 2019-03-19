d913a8118208daf983857258bb5485abe7688af1
luozuda <43851221+luozuda@users.noreply.github.com>
2019-03-20 00:29:06 +0800

    luozuda

--- vue/online-translation/src/App.vue
+++ vue/online-translation/src/App.vue
@@ -51,7 +51,7 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 4rem;
   overflow: hidden;
 }
 </style>
--- vue/online-translation/src/components/TranslateForm.vue
+++ vue/online-translation/src/components/TranslateForm.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="row" id="translateForm">
-    <form v-on:submit="formSubmit">
-      <input class="form-control" type="text" v-model="textToTranslate" placeholder="输入需要翻译的内容">
-      <select class="form-control" v-model="language">
-        <option value="en">English</option>
-        <option value="ru">Russian</option>
-        <option value="ko">Korean</option>
-        <option value="ja">Janpenese</option>
-      </select>
-      <input class="btn btn-primary" type="submit" value="翻译">
-    </form>
+  <div class="container" id="translateForm">
+    <div class="row">
+      <form v-on:submit="formSubmit" class="col-sm-6">
+        <input class="form-control" type="text" v-model="textToTranslate" placeholder="输入需要翻译的内容">
+        <select class="form-control" v-model="language">
+          <option value="en">English</option>
+          <option value="ru">Russian</option>
+          <option value="ko">Korean</option>
+          <option value="ja">Janpenese</option>
+        </select>
+        <input class="btn btn-primary" type="submit" value="翻译">
+      </form>
+    </div>
   </div>
 </template>
 
@@ -24,7 +26,6 @@ export default {
   },
   methods: {
     formSubmit: function(e) {
-      //   alert(this.textToTranslate);
       this.$emit("formSubmit", this.textToTranslate, this.language);
       e.preventDefault();
     }
